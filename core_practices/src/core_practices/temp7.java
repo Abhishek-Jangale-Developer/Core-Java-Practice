51fcc2e48c47c7748a1405bd0fdf78692c1e1e03
Abhishek-Jangale-Developer <abhishekjangle60@gmail.com>
2026-08-03 11:43:39 +0530

    seven commit

--- core_practices/src/core_practices/temp7.java
+++ core_practices/src/core_practices/temp7.java
@@ -2,21 +2,26 @@ package core_practices;
 
 public class temp7 {
 
-	public static void main(String[] args) {
-		Aeroplane first = new Aeroplane();
-		
-		first.takeoff();
+    public static void main(String[] args) {
 
-	}
+        Aeroplane one = new Aeroplane();
+        Aeroplane two = new Aeroplane();
 
+        one.pilot = "ABC";
+        two.pilot = "DEF";
+
+        one.takeoff();
+        two.takeoff();
+    }
 }
 
-class Aeroplane
-{
-	static String name ="AIR_INDIA";
-     
-  public void takeoff()
-  {
-	  System.out.println(name);
-  }
+class Aeroplane {
+
+    String name = "AIR_INDIA";
+    String pilot;
+
+    public void takeoff() {
+        System.out.println(name + " is taking off...");
+        System.out.println("Pilot: " + pilot);
+    }
 }
